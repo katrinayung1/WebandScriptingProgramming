335f2ff33ad9ef4c65230459ec1f818fdfe2cbbd
katrinayung1 <113392338+katrinayung1@users.noreply.github.com>
2022-09-29 14:26:00 -0500

    template

---
+++ Labs/Lab 1/LAB 1.js
@@ -0,0 +1,34 @@
+console.log("this is console");
+//const muHeading = document.querySelector("hi");
+document.querySelector("h1").textContent = "Hello World!";
+document.querySelector("p").textContent = "Hello World!";
+alert("This is an alern");
+prompt("enter your age");
+
+let abc; 
+abc = "chocolate";
+if (abc != "chocolate") {
+    alert("abc do not have the value chocolate");
+} else {
+    console.log("its true");
+}
+
+let number = 10;
+let myarray = ["a", 4, "b"];
+document.write(myarray[0]);
+document.write("<br>");
+let num1 = 5;
+let num2 = 7;
+let num3 = num1 * num2;
+document.write("the result of multiplication" + num3);
+document.write("<br");
+document.write(num1 + num2);
+document.write(myfunction(30,20));
+let output = myfunction(50,10);
+console.log(output);
+
+function myfunction(num1, num2) {
+    let num3 = num1/num2;
+    return num3;
+}
+document.querySelector("button").addEventListener("click", function)
